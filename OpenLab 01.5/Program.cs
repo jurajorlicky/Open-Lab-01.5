cd540e436e89506a0a32507d0c9d9f8f44e62bc1
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:21:02 +0100

    Add project files.

---
+++ OpenLab 01.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OpenLab_01._5
+{
+    class Program
+    {
+        public static Boolean Reverse(bool boolean)
+        {
+            return !boolean;
+        }
+        static void Main(string[] args)
+        {
+            bool boolean = false;
+            bool boolean_reversed = Reverse(boolean);
+            Console.WriteLine(boolean_reversed);
+            Console.ReadLine();
+        }
+    }
+}
+
+
